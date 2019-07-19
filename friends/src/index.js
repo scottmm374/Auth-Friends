5717526d7539d237a4d49efec395bb59847b5f6c
Michelle Scott <scottmm374@gmail.com>
2019-07-19 14:23:05 -0400

    save before taking break

--- friends/src/index.js
+++ friends/src/index.js
@@ -23,7 +23,9 @@ const store = createStore (
 )
 
 ReactDOM.render(<Provider store={store}>
-                    <App />,
+                     <BrowserRouter>
+                         <App />,
+                     </BrowserRouter> 
                 </Provider>,
                  document.getElementById('root'));
 
