df8915bae43afd33b0e13a31eaee3f0773a74262
Michelle Scott <scottmm374@gmail.com>
2019-07-19 13:55:29 -0400

    added index.js createstore logger provider

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import {Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const logger = (store) => (next) => (action) => {
+    console.log("previous state", store.getState())
+    console.log("Action", action)
+    next(action)
+    console.log("New state", store.getState())
+}
+
+const store = createStore (
+    reducer, compose(applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f, ),
+)
+
+ReactDOM.render(<Provider store={store}>
+                    <App />,
+                </Provider>,
+                 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
