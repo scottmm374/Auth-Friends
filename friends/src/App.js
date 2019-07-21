af1864d6569dd0a9110fc399c58dc30cadd0a579
Michelle Scott <scottmm374@gmail.com>
2019-07-21 17:34:44 -0400

    added variables for getfriend status

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <PrivateRoute exact path="/" component={Dashboard} />
+      <Route exact path="/login" compoenent={Login} />
     </div>
   );
 }
--- friends/src/actions.js
+++ friends/src/actions.js
@@ -2,6 +2,27 @@ import axios from 'axios';
 export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
+export const GET_FRIENDS_START = 'GET_FRIENDS_START';
+export const GET_FRIENDS_SUCCESS = 'GET_FRIENDS_SUCCESS';
+export const GET_FRIENDS_FAILED = 'GET_FRIENDS_FAILED';
+
+export function getFriends () {
+    return (dispatch) => {
+        dispatch({ type: GET_FRIENDS_START})
+        const headers = {
+            Authorization: localStorage.getItem('token'),
+        }
+
+        axios.get('', {headers})
+            .then((response) => {
+                dispatch({ type: GET_FRIENDS_SUCCESS, payload: response.data})
+            })
+            .catch((error) => {
+                dispatch({ type: GET_FRIENDS_FAILED, payload: error.response.data})
+            })
+    }
+}
+
 
 
 export function login(username, password) {
@@ -11,7 +32,7 @@ export function login(username, password) {
         return 
         axios.post('http//localhost:5000/api/login', {username, password })
             .then((response) => {
-                localStorage.setItem('token', res.data.token)
+                localStorage.setItem('token', response.data.token)
                 dispatch({ type: LOGIN_SUCCESS })
             })
             .catch((error) => {
